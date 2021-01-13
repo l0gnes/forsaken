70a544282958d0cb32ab47be68f2a5ed9a30708e
Alexander <40839690+boopdev@users.noreply.github.com>
2021-01-13 09:51:33 -0400

    Added Camera Movement

---
+++ mcm.py
@@ -0,0 +1,29 @@
+import pygame
+from player import PlayerObject
+
+class MovingCameraManager(object):
+    def __init__(self, game, s : pygame.Surface, *args, **kwargs):
+        self.game = game
+        self.mapped_surface = s
+
+        self.offset_x = 0
+        self.offset_y = 0
+
+    def render_bottom_level_mapping(self):
+        self.game.ACTIVE_SURFACE.SURFACE.blit(
+            self.mapped_surface,
+            (
+                0 - self.offset_x,
+                0 - self.offset_y
+            )
+        )
+
+    def render_character(self, char):
+        # Call this post-render_bottom_level_mapping
+        self.game.ACTIVE_SURFACE.SURFACE.blit(
+            char,
+            (
+                char.x - self.offset_x,
+                char.y - self.offset_y
+            )
+        )
